88afbfeb760308510d32096f477c566ce25a5c4b
andkymon <andkymon@gmail.com>
2025-02-06 19:31:04 +0800

    Add Tree class with buildTree and prettyPrint

---
+++ main.js
@@ -0,0 +1,5 @@
+import { Tree } from "./tree.js";
+
+const test = new Tree([3, 4, 2, 1, 7, 8, 6, 5, 9]);
+
+test.prettyPrint(test.root);
--- tree.js
+++ tree.js
@@ -1,5 +1,38 @@
 import { Node } from "./node.js";
 
 export class Tree {
-    
+    constructor(array) {
+        array.sort();
+        this.root = this.buildTree(array);
+        console.log(this.root);
+    }
+
+    buildTree(array) {
+        if (array.length === 0) {
+            return null;
+        }
+        
+        const middleIndex = Math.floor((array.length - 1) / 2);
+        const root = array[middleIndex];
+        const leftSubtree = this.buildTree(array.slice(0, middleIndex));
+        const rightSubtree = this.buildTree(array.slice(middleIndex + 1));
+
+        return new Node(root, leftSubtree, rightSubtree);
+    }
+
+    prettyPrint(node, prefix = "", isLeft = true) {
+        if (node === null) {
+            return;
+        }
+
+        if (node.rightNode !== null) {
+            this.prettyPrint(node.rightNode, `${prefix}${isLeft ? "│   " : "    "}`, false);
+        }
+
+        console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
+        
+        if (node.leftNode !== null) {
+            this.prettyPrint(node.leftNode, `${prefix}${isLeft ? "    " : "│   "}`, true);
+        }
+    }
 }

a65d030b333e6ed8dc530155797cf39556a16aa5
andkymon <andkymon@gmail.com>
2025-02-07 16:36:23 +0800

    Refactor code to improve readability
    - Reduced repeated logic
    - Added comments for clarity

--- tree.js
+++ tree.js
@@ -78,7 +78,7 @@ export class Tree {
     let targetNode = this.root;
     let targetParent;
 
-    // Stop on the target node
+    // Find the target node
     while (targetNode !== null) {
       if (value === targetNode.value) {
         break;
@@ -102,83 +102,60 @@ export class Tree {
       console.error("Node not found");
       return;
     }
-
-    if (targetParent.leftNode === targetNode) {
-      // If target node has no child, parent points to null
-      if (targetNode.leftNode === null && targetNode.rightNode === null) {
+    
+    // Case 1: If target node has no child, target node is replaced by null
+    if (targetNode.leftNode === null && targetNode.rightNode === null) {
+      
+      // Check which branch of the target node's parent references it, then set it to null
+      if (targetParent.leftNode === targetNode) {
         targetParent.leftNode = null;
-        return;
-      }
-
-      // If target node has one child, parent points to target node's child
-      if (targetNode.leftNode === null) {
-        targetParent.leftNode = targetNode.rightNode;
-        return;
-      } else if (targetNode.rightNode === null) {
-        targetParent.leftNode = targetNode.leftNode;
-        return;
-      }
-
-      // If target node has two children, find the inorder succeessor (left most node of right subtree)
-      let inorderSuccessor = targetNode.rightNode;
-      let inorderSuccessorParent = targetNode;
-
-      // If right node of target node is immediately the inorder successor, copy its value and right subtree to the target node
-      if (inorderSuccessor.leftNode === null) {
-        targetNode.value = inorderSuccessor.value;
-        targetNode.rightNode = inorderSuccessor.rightNode;
-        return;
-      }
-
-      while (inorderSuccessor.leftNode !== null) {
-        inorderSuccessorParent = inorderSuccessor;
-        inorderSuccessor = inorderSuccessor.leftNode;
-      }
-
-      // Copy inorder successor's value to target node
-      targetNode.value = inorderSuccessor.value;
-
-      // Inorder successor's right subtree will be passed to it's parent's left node
-      inorderSuccessorParent.leftNode = inorderSuccessor.rightNode;
-    }
-
-    if (targetParent.rightNode === targetNode) {
-      // If target node has no child, parent points to null
-      if (targetNode.leftNode === null && targetNode.rightNode === null) {
+      } else {
         targetParent.rightNode = null;
-        return;
       }
-
-      // If target node has one child, parent points to target node's child
-      if (targetNode.leftNode === null) {
-        targetParent.rightNode = targetNode.rightNode;
-        return;
-      } else if (targetNode.rightNode === null) {
-        targetParent.rightNode = targetNode.leftNode;
-        return;
-      }
-
-      // If target node has two children, find the inorder succeessor (left most node of right subtree)
-      let inorderSuccessor = targetNode.rightNode;
-      let inorderSuccessorParent = targetNode;
-
-      // If right node of target node is immediately the inorder successor, copy its value and right subtree to the target node
-      if (inorderSuccessor.leftNode === null) {
-        targetNode.value = inorderSuccessor.value;
-        targetNode.rightNode = inorderSuccessor.rightNode;
-        return;
-      }
-
-      while (inorderSuccessor.leftNode !== null) {
-        inorderSuccessorParent = inorderSuccessor;
-        inorderSuccessor = inorderSuccessor.leftNode;
-      }
-
-      // Copy inorder successor's value to target node
-      targetNode.value = inorderSuccessor.value;
-
-      // Inorder successor's right subtree will be passed to it's parent's left node
-      inorderSuccessorParent.leftNode = inorderSuccessor.rightNode;
+      return;
     }
+
+    // Case 2: If target node has one child, target node is replaced by target's child
+    if (targetNode.leftNode === null || targetNode.rightNode === null) {
+      let targetChild;
+
+       // Find target's only child
+      if (targetNode.leftNode === null) {
+        targetChild = targetNode.rightNode;      
+      } else if (targetNode.rightNode === null) {
+        targetChild = targetNode.leftNode;      
+      }
+  
+      // Check which branch of the target node's parent references it, then set it to target's child
+      if (targetParent.leftNode === targetNode) {
+        targetParent.leftNode = targetChild;
+      } else {
+        targetParent.rightNode = targetChild;
+      }
+
+      return;
+    }
+
+    // Case 3.1: If target node has two children, find the inorder succeessor (left most node of right subtree)
+    let inorderSuccessor = targetNode.rightNode;
+    let inorderSuccessorParent = targetNode;
+
+    // Case 3.2: If right node of target node is immediately the inorder successor, copy its value and right subtree to the target node
+    if (inorderSuccessor.leftNode === null) {
+      targetNode.value = inorderSuccessor.value;
+      targetNode.rightNode = inorderSuccessor.rightNode;
+      return;
+    }
+
+    while (inorderSuccessor.leftNode !== null) {
+      inorderSuccessorParent = inorderSuccessor;
+      inorderSuccessor = inorderSuccessor.leftNode;
+    }
+
+    // Copy inorder successor's value to target node
+    targetNode.value = inorderSuccessor.value;
+
+    // Inorder successor's right subtree will be passed to it's parent's left node
+    inorderSuccessorParent.leftNode = inorderSuccessor.rightNode;
   }
 }
